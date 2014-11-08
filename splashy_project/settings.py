8b82fd8956d26896768eb13726982571245b6e86
Michelle Glauser <michelleglauser@gmail.com>
2014-11-07 20:34:25 -0800

    Removed excess path from staticfiles_dirs in settings.

--- splashy_project/settings.py
+++ splashy_project/settings.py
@@ -87,6 +87,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'splashy_app/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
