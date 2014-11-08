3e8a954d5f708d4ae3610785f2f0b97382e3db3c
Michelle Glauser <michelleglauser@gmail.com>
2014-11-07 21:10:56 -0800

    Change STATICFILES_STORAGE to default in settings.

--- splashy_project/settings.py
+++ splashy_project/settings.py
@@ -85,8 +85,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'splashy_project/staticfiles'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'splashy_app/static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
